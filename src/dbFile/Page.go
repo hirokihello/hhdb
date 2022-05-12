9d1dfd2d60bb45705d32ddf34ed36433a5ae022e
hirokihello <iammyeye1@gmail.com>
2022-05-13 00:38:53 +0900

    add page funcs and test

--- src/dbFile/Page.go
+++ src/dbFile/Page.go
@@ -1,25 +1,67 @@
 package dbFile
-import ("bytes");
 // import (
 // 	"unicode/utf8"
 // )
 // 文字こーどの現状での必要性がなさそうなので放置
 
-type Page struct {
-	ByteBuffer bytes.Buffer;
+import (
+	"encoding/binary"
+)
+type PageI interface {
+	SetString();
+	SetInt();
+	GetInt();
+	GetString();
 }
 
-func getString (a []byte, offset int) string {
-	bytes := a[offset:];
-	return string(bytes);
+type Page struct {
+	ByteBuffer []byte;
 }
+
+// マジックナンバー４使いがち...
+
+func (a Page) GetString (offset int) string {
+	size := a.GetInt(offset);
+	start := offset+4;
+	return string(a.ByteBuffer[start:start+size]);
+}
+
+func (a Page) GetInt (offset int) int {
+	return int(binary.LittleEndian.Uint32(a.ByteBuffer[offset:offset+4]));
+}
+
+func (a Page) SetInt (num uint32, offset int) {
+	// int max number
+	if(num > 2147483647) {
+		num = 2147483647;
+	}
+	// 最小値の対応とかも今後していきたい
+	bs := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, num)
+
+	a.ByteBuffer[offset] = bs[0];
+	a.ByteBuffer[offset+1] = bs[1];
+	a.ByteBuffer[offset+2] = bs[2];
+	a.ByteBuffer[offset+3] = bs[3];
+}
+
+func (a Page) SetString (str string, offset int) {
+	bs := []byte(str);
+	length := len(bs);
+	a.SetInt(uint32(length), offset);
+
+	offset += 4;
+	for index, value := range bs {
+		a.ByteBuffer[offset+index] = value;
+	}
+}
+
 
 func CreatePage (size int) Page {
-	newBuff := bytes.NewBuffer(make([]byte, size));
-	return Page{ByteBuffer: *newBuff};
+	newBuff := make([]byte, size);
+	return Page{ByteBuffer: newBuff};
 }
 
 func LoadBufferToPage (initialBytes []byte) Page {
-	newBuff := bytes.NewBuffer(initialBytes);
-	return Page{ByteBuffer: *newBuff}
+	return Page{ByteBuffer: initialBytes}
 }
--- src/dbFile/Test_test.go
+++ src/dbFile/Test_test.go
@@ -1,35 +1,66 @@
 package dbFile;
 
 import "testing";
-import ("bytes");
+import (
+	"encoding/binary"
+	"bytes"
+)
 
 func TestBlock (t *testing.T) {
 	blk := Block{FileName: "testBlockName", BlockNumber: 1};
-	// blk := CreateBlock("testBlockName", 1);
 	if(blk.ToString() != "filename: testBlockName, block: 1") {
 		t.Errorf("blk.ToString() = [%v], want: [%v]", blk.ToString(), "filename: testBlockName, block: 1")
 	}
 }
 
-// func TestPage (t *testing.T) {
-// 	blk := Block{FileName: "testBlockName", BlockNumber: 1};
-// 	// blk := CreateBlock("testBlockName", 1);
-// 	if(blk.ToString() != "filename: testBlockName, block: 1") {
-// 		t.Errorf("blk.ToString() = [%v], want: [%v]", blk.ToString(), "filename: testBlockName, block: 1")
-// 	}
-// }
-
 func TestPage (t *testing.T) {
 	page := CreatePage(3);
 
-	if(page.ByteBuffer.String()!= bytes.NewBuffer(make([]byte, 3)).String()) {
+	if(bytes.Compare(page.ByteBuffer, make([]byte, 3)) != 0) {
 		t.Errorf("buffer is not created properly")
 	}
 
 	var sample string = "it is for LoadBufferToPage func";
 	loadedPage := LoadBufferToPage([]byte(sample));
 
-	if(sample != loadedPage.ByteBuffer.String()) {
+	if(sample != string(loadedPage.ByteBuffer)) {
 		t.Errorf("page was not correctly loaded");
+	}
+}
+
+func TestPageGetInt (t *testing.T) {
+	bs := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, 2147483647)
+	page := LoadBufferToPage(bs);
+
+	// t.Errorf(string(page.ByteBuffer));
+	if(len(page.ByteBuffer) > 4) {
+		t.Errorf("length of bytes is[%v]", len(page.ByteBuffer))
+	}
+
+	if(int32(binary.LittleEndian.Uint32(page.ByteBuffer)) != 2147483647) {
+		t.Errorf("number [%v] is wrong", int32(binary.LittleEndian.Uint64(page.ByteBuffer)) )
+	}
+}
+
+func TestPgeSetInt (t *testing.T) {
+	bs := []byte("fasjfauifnauifnwefbwebfiwbvvfavvdavav");
+	page := LoadBufferToPage(bs);
+
+	page.SetInt(1234, 4);
+
+	if(page.GetInt(4) != 1234) {
+		t.Errorf("blk.ToString() = [%v], want: [%v]", page.GetInt(4), 1234)
+	}
+}
+
+func TestPageSetString (t *testing.T) {
+	bs := []byte("fasjfauifnauifnwefbwebfiwbvvfavvdavav");
+	page := LoadBufferToPage(bs);
+
+	page.SetString("1234ABCD", 4);
+
+	if(page.GetString(4) != "1234ABCD") {
+		t.Errorf("blk.ToString() = [%v], want: [%v]", page.GetString(4), "1234ABCD")
 	}
 }
