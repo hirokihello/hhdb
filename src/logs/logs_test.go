9549ec8ddfc5349b18b1bef3546983cf2d23f1ce
hirokihello <iammyeye1@gmail.com>
2022-05-21 15:25:57 +0900

    fix log files and test

--- src/logs/logs_test.go
+++ src/logs/logs_test.go
@@ -1,57 +1,58 @@
-package logs;
+package logs
 
 import (
-	"testing"
-	"strconv"
 	"fmt"
-);
+	"strconv"
+	"testing"
 
-import "github.com/hirokihello/hhdb/src/files"
+	"github.com/hirokihello/hhdb/src/files"
+	"github.com/stretchr/testify/assert"
+)
 
-var fileManager *files.Manager;
-var logManager Manager;
+var fileManager *files.Manager
+var logManager Manager
 
 func TestMain(m *testing.M) {
-	fileManager = files.CreateManager("test_dir", 400);
-	logManager = *CreateLogManager(fileManager, "test_logfile");
-	fmt.Printf("blockN is")
-	fmt.Println(logManager.CurrentBlock.BlockNumber);
+	fileManager = files.CreateManager("test_dir", 400)
+	logManager = *CreateLogManager(fileManager, "test_logfile")
 	m.Run()
 }
 
-func showLogRecords () {
-	itr := *logManager.Iterator();
+func showLogRecords() {
+	itr := *logManager.Iterator()
 	// for i := 0; itr.HasNext() && i < 100; i++ {
 	for itr.HasNext() {
-		records := itr.Next();
-		page := files.LoadBufferToPage(records);
-		s := page.GetString(0);
-		npos := files.MaxLengthOfStringOnPage(s);
-		value := page.GetInt(npos);
-		fmt.Println("[" + s + ", " + strconv.Itoa(value) + "]");
+		records := itr.Next()
+		page := files.LoadBufferToPage(records)
+		s := page.GetString(0)
+		npos := files.MaxLengthOfStringOnPage(s)
+		value := page.GetInt(npos)
+		fmt.Println("[" + s + ", " + strconv.Itoa(value) + "]")
 	}
 }
 
-func createLogRecords (num int) {
-	str := "record: " + strconv.Itoa(num);
+func createLogRecords(num int) {
+	str := "record: " + strconv.Itoa(num)
 	size := files.MaxLengthOfStringOnPage(str)
 	// stringの長さ+integer
-	new_log := make([]byte, size + 4);
+	new_log := make([]byte, size+4)
 	page := files.LoadBufferToPage(new_log)
-	page.SetString(str, 0);
-	page.SetInt(uint32(num), size);
-	logManager.Append(page.Contents());
+	page.SetString(str, 0)
+	page.SetInt(uint32(num), size)
+	logManager.Append(page.Contents())
 }
 
-func TestLogManager (t *testing.T) {
+func TestLogManager(t *testing.T) {
+	assert.Equal(t, logManager.CurrentBlock.BlockNumber, 0)
 	for i := 0; i < 20; i++ {
+		createLogRecords(i)
+	}
+	assert.Equal(t, logManager.CurrentBlock.BlockNumber, 1)
+	showLogRecords()
+	for i := 35; i < 70; i++ {
 		createLogRecords(i);
 	}
-	showLogRecords()
-	// for i := 35; i < 70; i++ {
-	// 	createLogRecords(i);
-	// }
 	fmt.Println("")
-	// logManager.Flush(65);
-	// showLogRecords();
+	logManager.Flush(65);
+	showLogRecords();
 }
